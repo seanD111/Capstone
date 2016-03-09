a2d9d58286457a2db6a7c1a6d16e049a91216e58
seanD111 <sean_delong@hotmail.ca>
2016-03-09 01:32:22 -0500

    added phonemes to data; added more to gui

--- graphTest.m
+++ graphTest.m
@@ -22,7 +22,7 @@ function varargout = graphTest(varargin)
 
 % Edit the above text to modify the response to help graphTest
 
-% Last Modified by GUIDE v2.5 03-Mar-2016 20:53:19
+% Last Modified by GUIDE v2.5 08-Mar-2016 21:20:26
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -275,3 +275,21 @@ function listbox1_CreateFcn(hObject, eventdata, handles)
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
 end
+
+
+% --- Executes on button press in checkbox1.
+function checkbox1_Callback(hObject, eventdata, handles)
+% hObject    handle to checkbox1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hint: get(hObject,'Value') returns toggle state of checkbox1
+
+
+% --- Executes on button press in checkbox2.
+function checkbox2_Callback(hObject, eventdata, handles)
+% hObject    handle to checkbox2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hint: get(hObject,'Value') returns toggle state of checkbox2
---
+++ refreshCorpusTable.m
@@ -0,0 +1,9 @@
+function [ output_args ] = refreshCorpusTable( tableHandle )
+%REFRESHCORPUSTABLE Display all the corpus directories and languages in the inputted table handle
+%   Call the function with the table handle you wish to update
+corpusDir='data/corpus';
+
+
+
+end
+
