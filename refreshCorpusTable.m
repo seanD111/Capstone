fab7f5b18bb4c2c347060355fcca3b8618701554
seanD111 <sean_delong@hotmail.ca>
2016-03-16 11:29:15 -0400

    updating corpus list function

--- refreshCorpusTable.m
+++ refreshCorpusTable.m
@@ -1,8 +1,33 @@
 function [ output_args ] = refreshCorpusTable( tableHandle )
 %REFRESHCORPUSTABLE Display all the corpus directories and languages in the inputted table handle
 %   Call the function with the table handle you wish to update
-corpusDir='data/corpus';
+rootDir='data/corpus/';
+dirlist=dir(rootDir)
+corpusList=struct('name', {}, 'langCount', [], 'languages', {}, 'clipCount', [])
 
+for i=1:length(dirlist)
+    if dirlist(i).name(1)~='.'
+        corpusList(end+1, 1).name=dirlist(i).name
+        corpDir=dir(strcat(rootDir, corpusList(end, 1).name))
+        
+        for j=1:length(corpDir)
+            if corpDir(j).name(1)~='.'
+                corpusList(end, 1).languages{end+1, 1}=corpDir(j).name
+                
+                
+                
+            end
+        end
+        
+    end
+end
+
+for i=1:length(corpusList)
+    [corpusList(i).langCount, ~]=size(corpusList(i).languages)
+    fileList = getAllFiles(strcat(rootDir, corpusList(i).name));
+    [corpusList(i).clipCount, ~]=size(fileList)
+
+end
 
 
 end
