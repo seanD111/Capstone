ecd62454003faabae9120825d1fda830aee5008e
seanD111 <sean_delong@hotmail.ca>
2016-03-16 20:30:12 -0400

    refresh corpus table function was completed

--- refreshCorpusTable.m
+++ refreshCorpusTable.m
@@ -1,18 +1,18 @@
-function [ output_args ] = refreshCorpusTable( tableHandle )
+function [ tableHandle_out ] = refreshCorpusTable( tableHandle )
 %REFRESHCORPUSTABLE Display all the corpus directories and languages in the inputted table handle
 %   Call the function with the table handle you wish to update
 rootDir='data/corpus/';
-dirlist=dir(rootDir)
-corpusList=struct('name', {}, 'langCount', [], 'languages', {}, 'clipCount', [])
-
+dirlist=dir(rootDir);
+corpusList=struct('name', {}, 'langCount', [], 'languages', {}, 'clipCount', []);
+allFiles=getAllFiles(rootDir);
 for i=1:length(dirlist)
     if dirlist(i).name(1)~='.'
-        corpusList(end+1, 1).name=dirlist(i).name
-        corpDir=dir(strcat(rootDir, corpusList(end, 1).name))
+        corpusList(end+1, 1).name=dirlist(i).name;
+        corpDir=dir(strcat(rootDir, corpusList(end, 1).name));
         
         for j=1:length(corpDir)
             if corpDir(j).name(1)~='.'
-                corpusList(end, 1).languages{end+1, 1}=corpDir(j).name
+                corpusList(end, 1).languages{end+1, 1}=corpDir(j).name;
                 
                 
                 
@@ -23,9 +23,15 @@ for i=1:length(dirlist)
 end
 
 for i=1:length(corpusList)
-    [corpusList(i).langCount, ~]=size(corpusList(i).languages)
-    fileList = getAllFiles(strcat(rootDir, corpusList(i).name));
-    [corpusList(i).clipCount, ~]=size(fileList)
+    [corpusList(i).langCount, ~]=size(corpusList(i).languages);
+    
+    for j=1:length(corpusList(i).languages)
+
+        fileList = getAllFiles(strcat(rootDir, corpusList(i).name,'/',corpusList(i).languages{j},'/'));
+        corpusList(i).clipCount(j,1)=length(fileList);
+    end
+    
+
 
 end
 
