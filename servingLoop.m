8e4f4cee92999f04d77f30dee220c782b2406fd0
seanD111 <sean_delong@hotmail.ca>
2016-04-27 22:05:05 -0400

    made directory structure fixed

--- servingLoop.m
+++ servingLoop.m
@@ -3,19 +3,24 @@ y=0;
 while y >= 0
     
     incoming_data=getAllFiles('incoming_data/');
-    if ~isempty(incoming_data)        
-        try
-            [signal, fs]=audioread(incoming_data{1,1});
-            [predictRatios, predictLanguages]=give_prediction(signal, fs, 7);
-            
-            save(strcat('data/user_inputs/', incoming_data{1,1}, '.mat'), 'predictRatios', 'predictLanguages', 'signal', 'fs')
-            delete(incoming_data{1,1})
-                [sortedRatios, sortI]=sort(predictRatios, 'descend');
-                x.ratios=sortedRatios;
-                x.languages=predictLanguages(sortI);
-                savejson('',x,'matlab_node/matlabData.json');
-        catch err
-        end
-    end
+    for i = 1:length(incoming_data)
+         if ~isempty(incoming_data) 
+            if isempty(strfind(incoming_data{i, 1}, '.gitignore'))
+                                        
+                try
+                    [signal, fs]=audioread(incoming_data{i,1});
+                    [predictRatios, predictLanguages]=give_prediction(signal, fs, 7);
 
+                    save(strcat('data/user_inputs/', incoming_data{i,1}, '.mat'), 'predictRatios', 'predictLanguages', 'signal', 'fs')
+                    delete(incoming_data{i,1})
+                        [sortedRatios, sortI]=sort(predictRatios, 'descend');
+                        x.ratios=sortedRatios;
+                        x.languages=predictLanguages(sortI);
+                        savejson('',x,'matlab_node/matlabData.json');
+                catch err
+                end
+            end
+        end
+        
+    end
 end
